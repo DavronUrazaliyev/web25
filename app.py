4fa6d273a3876dd80def4163da37e06d25d87241
Davron17 <dorazaliyev75@gmail.com>
2024-01-30 12:35:29 +0500

    error

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 import telegram
-from telegram import Update
-from telegram.ext import CallbackContext, MessageHandler, Filters, Updater
-
+# Create the application instance
 app = Flask(__name__)
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = '6440042136:AAGp50G4rRElB42wkDg7DlM91-opDBQwjhc'
-bot = telegram.Bot(token=TOKEN)
+TOKEN ='6440042136:AAGp50G4rRElB42wkDg7DlM91-opDBQwjhc'
+bot = telegram.Bot(TOKEN)
+chat_id = '5271463532'
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    chat_id = update.message.chat_id
-    text = update.message.text
-    bot.send_message(chat_id=chat_id, text=text)  # Fix the syntax error here
-
+# route for index page
 @app.route('/', methods=['POST'])
 def index():
-    return 'Hello from Flask!'
-
-if __name__ == "__main__":
-    updater = Updater(token=TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    updater.start_polling()
-    app.run(debug=True)
-    updater.idle()
+    print('index page')
+    bot.send_message(chat_id=chat_id, text='Hello World!!!')
+    return 'index page'
