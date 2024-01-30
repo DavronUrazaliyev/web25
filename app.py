23cca935418b3f79edbda86ec4e2487333f8db98
Davron17 <dorazaliyev75@gmail.com>
2024-01-30 14:54:32 +0500

    30

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
-from flask import Flask
-import telegram
-# Create the application instance
+from flask import Flask, request
+import telegram  
+from telegram import Update
+from telegram.ext import CallbackContext, MessageHandler, Filters, Updater
+
+
+
 app = Flask(__name__)
 
 TOKEN ='6440042136:AAGp50G4rRElB42wkDg7DlM91-opDBQwjhc'
 bot = telegram.Bot(TOKEN)
 chat_id = '5271463532'
 URL = 'https://davron17.pythonanywhere.com/'
-Remove webhook
+
+
 bot.delete_webhook()
 bot.setWebhook(URL)
 # route for index page
 @app.route('/', methods=['POST'])
-def index():
+def index(update: Update, context: CallbackContext):
     print('index page')
     bot.send_message(chat_id=chat_id, text='Hello World!!!')
     return 'index page'
+    chat_id = update.message.chat_id
+    text = update.message.text
+    
+    bot.send_message(chat_id=chat_id, text=text)
 
+if __name__ == "__main__":
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
 
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    app.run(debug=True)
+    updater.idle()
 print(bot.get_webhook_info())
